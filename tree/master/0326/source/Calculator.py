da236aa7efcb16d953e0c292511afc1d21b57ab0
Garen0512 <37055420+Garen0512@users.noreply.github.com>
2018-03-29 16:50:00 +0800

    OK

---
+++ tree/master/0326/source/Calculator.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[21]:
+
+
+def add(*args):
+    return args[0]+args[1]
+def subtract(*args):
+    return args[0]-args[1]
+def multiply(*args):
+    return args[0]*args[1]
+def divide(*args):
+    return args[0]/args[1]
+
